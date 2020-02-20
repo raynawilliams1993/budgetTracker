c05d2dd3beaebfe181156e4a9d85d0cb1df0107e
rayna williams <Raynawilliams123@gmail.com>
2020-02-20 15:50:34 -0700

    everything is working online and offline

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+      navigator.serviceWorker.register("service-worker.js").then(reg => {
+          console.log("We found your service worker file!", reg);
+      });
+  });
+  console.log("service-worker")
+}
+
 let transactions = [];
 let myChart;
+
+
 
 fetch("/api/transaction")
   .then(response => response.json())
---
+++ public/server-worker.js
@@ -0,0 +1,55 @@
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.js',
+    'style.css'
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
+self.addEventListener("install", function (evt) {
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    // self.skipWaiting();
+});
+
+// self.addEventListener("activate", function (evt) {
+//     evt.waitUntil(
+//         caches.keys().then(keyList => {
+//             return Promise.all(
+//                 keyList.map(key => {
+//                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+//                         console.log("Removing old cache data", key);
+//                         return caches.delete(key);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+
+//     self.clients.claim();
+// });
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
+
+    evt.respondWith(
+        fetch(evt.request).catch(function () {
+            return caches.match(evt.request).then(function (res) {
+                if (res) {
+                    return res;
+                } else if (evt.request.headers.get("accept").includes("text/html")) {
+                    return caches.match("/index.html")
+                }
+            })
+        })
+    )
+});
